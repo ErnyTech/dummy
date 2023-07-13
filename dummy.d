fa42de81c91e13bf0586c1fa2c59a367dd1afcd0
Ernesto Castellotti <mail@ernestocastellotti.it>
2023-07-13 16:29:25 +0200

    Add arm32 support

--- dummy.d
+++ dummy.d
@@ -6,23 +6,33 @@
 import gcc.attributes : register, always_inline;
 
 version (X86_64) {
+    alias clong = long; // long == 64bit
     enum YIELD = 24;
     enum PAUSE = 34;
     enum NANOSLEEP = 35;
     enum HAVE_PAUSE = true;
+} else version (ARM) {
+    alias clong = int;  // long == 32 bit
+    enum YIELD = 158;
+    enum PAUSE = 29;
+    enum NANOSLEEP = 162;
+    enum HAVE_PAUSE = true;
 } else version (AArch64) {
+    alias clong = long; // long == 64 bit
     enum YIELD = 124;
     enum PPOLL = 73;
     enum NANOSLEEP = 101;
-    enum HAVE_PAUSE = false;    // aarch64 doesn't have pause, need to use ppoll
+    enum HAVE_PAUSE = false;
+} else version (ARM_Thumb) {
+    static assert(0, "ARM Thumb mode not supported.");
 } else {
     static assert(0, "CPU architecture not supported.");
 }
 
-long syscall0(long num) @always_inline {
+clong syscall0(clong num) @always_inline {
     version (X86_64) {
-        long ret;
-        @register("rax") long _num = num;
+        clong ret = void;
+        @register("rax") clong _num = num;
 
         asm {
             "syscall"
@@ -32,9 +42,22 @@ long syscall0(long num) @always_inline {
         }
 
         return ret;
+    } else version (ARM) {
+        @register("r7") clong _num = num;
+        @register("r0") clong _arg1 = void;
+
+        asm {
+            "svc #0"
+            : "=r"(_arg1), "=r"(_num)
+            : "r"(_arg1),
+              "r"(_num)
+            : "memory", "cc", "lr";
+        }
+
+        return _arg1;
     } else version (AArch64) {
-        @register("x8") long _num = num;
-        @register("x0") long _arg1;
+        @register("x8") clong _num = num;
+        @register("x0") clong _arg1 = void;
 
         asm {
             "svc #0"
@@ -47,11 +70,11 @@ long syscall0(long num) @always_inline {
     }
 }
 
-long syscall1(long num, long arg1) @always_inline {
+clong syscall1(clong num, clong arg1) @always_inline {
     version (X86_64) {
-        long ret;
-        @register("rax") long _num = num;
-        @register("rdi") long _arg1 = arg1;
+        clong ret = void;
+        @register("rax") clong _num = num;
+        @register("rdi") clong _arg1 = arg1;
 
         asm {
             "syscall"
@@ -62,9 +85,22 @@ long syscall1(long num, long arg1) @always_inline {
         }
 
         return ret;
+    } else version (ARM) {
+        @register("r7") clong _num = num;
+        @register("r0") clong _arg1 = arg1;
+
+        asm {
+            "svc #0"
+            : "=r"(_arg1), "=r"(_num)
+            : "r"(_arg1),
+              "r"(_num)
+            : "memory", "cc", "lr";
+        }
+
+        return _arg1;
     } else version (AArch64) {
-        @register("x8") long _num = num;
-        @register("x0") long _arg1 = arg1;
+        @register("x8") clong _num = num;
+        @register("x0") clong _arg1 = arg1;
 
         asm {
             "svc #0"
@@ -78,14 +114,14 @@ long syscall1(long num, long arg1) @always_inline {
     }
 }
 
-long syscall4(long num, long arg1, long arg2, long arg3, long arg4) @always_inline {
+clong syscall4(clong num, clong arg1, clong arg2, clong arg3, clong arg4) @always_inline {
     version (X86_64) {
-        long ret;
-        @register("rax") long _num = num;
-        @register("rdi") long _arg1 = arg1;
-        @register("rsi") long _arg2 = arg2;
-        @register("rdx") long _arg3 = arg3;
-        @register("r10") long _arg4 = arg4;
+        clong ret = void;
+        @register("rax") clong _num = num;
+        @register("rdi") clong _arg1 = arg1;
+        @register("rsi") clong _arg2 = arg2;
+        @register("rdx") clong _arg3 = arg3;
+        @register("r10") clong _arg4 = arg4;
 
         asm {
             "syscall"
@@ -96,12 +132,28 @@ long syscall4(long num, long arg1, long arg2, long arg3, long arg4) @always_inli
         }
 
         return ret;
+    } else version (ARM) {
+        @register("r7") clong _num = num;
+        @register("r0") clong _arg1 = arg1;
+        @register("r1") clong _arg2 = arg2;
+        @register("r2") clong _arg3 = arg3;
+        @register("r3") clong _arg4 = arg4;
+
+        asm {
+            "svc #0"
+            : "=r"(_arg1), "=r"(_num)
+            : "r" (_arg1), "r" (_arg2), "r" (_arg3), "r" (_arg4),
+              "r"(_num)
+            : "memory", "cc", "lr";
+        }
+
+        return _arg1;
     } else version (AArch64) {
-        @register("x8") long _num = num;
-        @register("x0") long _arg1 = arg1;
-        @register("x1") long _arg2 = arg2;
-        @register("x2") long _arg3 = arg3;
-        @register("x3") long _arg4 = arg4;
+        @register("x8") clong _num = num;
+        @register("x0") clong _arg1 = arg1;
+        @register("x1") clong _arg2 = arg2;
+        @register("x2") clong _arg3 = arg3;
+        @register("x3") clong _arg4 = arg4;
 
         asm {
             "svc #0"
@@ -117,7 +169,7 @@ long syscall4(long num, long arg1, long arg2, long arg3, long arg4) @always_inli
 
 
 extern(C) void _start() {
-    long timesleep = 60;
+    clong timesleep = 60;
 
     while (true) {
         // YIELD!
@@ -132,6 +184,6 @@ extern(C) void _start() {
 
         // How the fuck did you come here?
         // Sleep 60s in case of runaway loop (It shouldn't happen)
-        syscall1(NANOSLEEP, cast(long) &timesleep);
+        syscall1(NANOSLEEP, cast(clong) &timesleep);
     }
 }
