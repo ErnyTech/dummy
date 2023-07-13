0ad28e1f3a706731c0f038d95149c9b52868a73d
Ernesto Castellotti <mail@ernestocastellotti.it>
2023-07-13 02:00:26 +0200

    Add arm64 support

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # Copyright Ernesto Castellotti <ernesto@castellotti.net>
 # SPDX-License-Identifier: GPL-3.0-only
 
-FROM debian:12-slim AS builder
+FROM debian:trixie-slim AS builder
 MAINTAINER Ernesto Castellotti <mail@ernestocastellotti.it>
 
 COPY dummy.d ./
-RUN apt-get update && apt-get install -y gdc
-RUN gdc -fno-druntime -nodefaultlibs -nostartfiles -fno-stack-protector -static -O3 dummy.d -o dummy
+RUN apt-get update && apt-get install -y gdc-13
+RUN gdc-13 -fno-druntime -nodefaultlibs -nostartfiles -fno-stack-protector -static -O3 -ffunction-sections -Wl,--gc-sections dummy.d -o dummy
 
 FROM scratch
 MAINTAINER Ernesto Castellotti <mail@ernestocastellotti.it>
--- dummy.d
+++ dummy.d
@@ -3,39 +3,135 @@
     SPDX-License-Identifier: GPL-3.0-only
 */
 
-enum YIELD = 24;
-enum NANOSLEEP = 35;
-enum PAUSE = 34;
+import gcc.attributes : register, always_inline;
+
+version (X86_64) {
+    enum YIELD = 24;
+    enum PAUSE = 34;
+    enum NANOSLEEP = 35;
+    enum HAVE_PAUSE = true;
+} else version (AArch64) {
+    enum YIELD = 124;
+    enum PPOLL = 73;
+    enum NANOSLEEP = 101;
+    enum HAVE_PAUSE = false;    // aarch64 doesn't have pause, need to use ppoll
+} else {
+    static assert(0, "CPU architecture not supported.");
+}
+
+long syscall0(long num) @always_inline {
+    version (X86_64) {
+        long ret;
+        @register("rax") long _num = num;
+
+        asm {
+            "syscall"
+            : "=a" (ret)
+            : "0" (_num)
+            : "rcx", "r11", "memory", "cc";
+        }
+
+        return ret;
+    } else version (AArch64) {
+        @register("x8") long _num = num;
+        @register("x0") long _arg1;
+
+        asm {
+            "svc #0"
+            : "=r" (_arg1)
+            : "r" (_num)
+            : "memory", "cc";
+        }
+
+        return _arg1;
+    }
+}
+
+long syscall1(long num, long arg1) @always_inline {
+    version (X86_64) {
+        long ret;
+        @register("rax") long _num = num;
+        @register("rdi") long _arg1 = arg1;
+
+        asm {
+            "syscall"
+            : "=a" (ret)
+            : "r" (_arg1),
+              "0" (_num)
+            : "rcx", "r11", "memory", "cc";
+        }
+
+        return ret;
+    } else version (AArch64) {
+        @register("x8") long _num = num;
+        @register("x0") long _arg1 = arg1;
+
+        asm {
+            "svc #0"
+            : "=r" (_arg1)
+            : "r" (_arg1),
+              "r"(_num)
+            : "memory", "cc";
+        }
+
+        return _arg1;
+    }
+}
+
+long syscall4(long num, long arg1, long arg2, long arg3, long arg4) @always_inline {
+    version (X86_64) {
+        long ret;
+        @register("rax") long _num = num;
+        @register("rdi") long _arg1 = arg1;
+        @register("rsi") long _arg2 = arg2;
+        @register("rdx") long _arg3 = arg3;
+        @register("r10") long _arg4 = arg4;
+
+        asm {
+            "syscall"
+            : "=a" (ret)
+            : "r" (_arg1), "r" (_arg2), "r" (_arg3), "r" (_arg4),
+              "0" (_num)
+            : "rcx", "r11", "memory", "cc";
+        }
+
+        return ret;
+    } else version (AArch64) {
+        @register("x8") long _num = num;
+        @register("x0") long _arg1 = arg1;
+        @register("x1") long _arg2 = arg2;
+        @register("x2") long _arg3 = arg3;
+        @register("x3") long _arg4 = arg4;
+
+        asm {
+            "svc #0"
+            : "=r" (_arg1)
+            : "r" (_arg1), "r" (_arg2), "r" (_arg3), "r" (_arg4),
+              "r" (_num)
+            : "memory", "cc";
+        }
+
+        return _arg1;
+    }
+}
+
 
 extern(C) void _start() {
     long timesleep = 60;
 
     while (true) {
-        ulong ret;
-
         // YIELD!
-        asm {
-            "syscall"
-            : "=a" (ret)
-            : "0" (cast(ulong) YIELD)
-            : "rcx", "r11", "memory";
-        }
+        syscall0(YIELD);
 
         // This should wait forever
-        asm {
-            "syscall"
-            : "=a" (ret)
-            : "0" (cast(ulong) PAUSE)
-            : "rcx", "r11", "memory";
+        static if (HAVE_PAUSE) {
+            syscall0(PAUSE);
+        } else {
+            syscall4(PPOLL, 0, 0, 0, 0);
         }
 
         // How the fuck did you come here?
         // Sleep 60s in case of runaway loop (It shouldn't happen)
-        asm {
-            "syscall"
-            : "=a" (ret)
-            : "0" (cast(ulong) NANOSLEEP), "D" (cast(ulong) &timesleep) // Seriously, don't copy this
-            : "rcx", "r11", "memory";
-        }
+        syscall1(NANOSLEEP, cast(long) &timesleep);
     }
 }
